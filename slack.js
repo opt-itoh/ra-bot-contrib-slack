d550d02b76aba67f44180bb155f9d1ba6d105f27
itoh <hs_ito@opt.co.jp>
2018-05-23 15:14:03 +0900

    first commit

---
+++ slack.js
@@ -0,0 +1,191 @@
+/**
+ * Copyright 2015 Adrian Lansdown
+ * Not created by, affiliated with, or supported by Slack Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+// https://github.com/yayadrian/node-red-slack/blob/master/slackpost.js
+// https://github.com/node-red/node-red/blob/master/nodes/core/io/21-httpin.js
+
+module.exports = function(RED) {
+    "use strict";
+
+    var request = require('request');
+
+    function createResponseWrapper(node,res) {
+        var wrapper = {
+            _res: res
+        };
+        var toWrap = [
+            "append",
+            "attachment",
+            "cookie",
+            "clearCookie",
+            "download",
+            "end",
+            "format",
+            "get",
+            "json",
+            "jsonp",
+            "links",
+            "location",
+            "redirect",
+            "render",
+            "send",
+            "sendfile",
+            "sendFile",
+            "sendStatus",
+            "set",
+            "status",
+            "type",
+            "vary"
+        ];
+        toWrap.forEach(function(f) {
+            wrapper[f] = function() {
+                node.warn(RED._("httpin.errors.deprecated-call",{method:"msg.res."+f}));
+                var result = res[f].apply(res,arguments);
+                if (result === res) {
+                    return wrapper;
+                } else {
+                    return result;
+                }
+            }
+        });
+        return wrapper;
+    }
+
+
+    function slackIn(n) {
+        RED.nodes.createNode(this,n);
+        if (RED.settings.httpNodeRoot !== false) {
+            if (!n.url) {
+                this.warn(RED._("httpin.errors.missing-path"));
+                return;
+            }
+            this.url = n.url;
+            if (this.url[0] !== '/') {
+                this.url = '/'+this.url;
+            }
+            this.method = 'post';
+
+            var node = this;
+
+            this.errorHandler = function(err,req,res,next) {
+                node.warn(err);
+                res.sendStatus(500);
+            };
+
+            this.callback = function(req,res) {
+                var msgid = RED.util.generateId();
+                res._msgid = msgid;
+                if (req.body.bot_id) {
+                    return res.status(200).send({});
+                }
+                node.send({_msgid:msgid,req:req,res:createResponseWrapper(node,res),payload:req.body});
+            };
+
+            RED.httpNode.post(this.url,this.callback,this.errorHandler);
+
+            this.on("close",function() {
+                var node = this;
+                RED.httpNode._router.stack.forEach(function(route,i,routes) {
+                    if (route.route && route.route.path === node.url && route.route.methods[node.method]) {
+                        routes.splice(i,1);
+                    }
+                });
+            });
+        } else {
+            this.warn(RED._("httpin.errors.not-created"));
+        }
+    }
+    RED.nodes.registerType("slack in",slackIn);
+
+    function slackOut(n) {
+        RED.nodes.createNode(this,n);
+        var node = this;
+        this.on("input",function(msg) {
+            if (msg.res) {
+                var headers = {};
+                var statusCode = 200;
+                if (typeof msg.payload == "object" && !Buffer.isBuffer(msg.payload)) {
+                    msg.res._res.status(statusCode).jsonp(msg.payload);
+                } else {
+                    if (msg.res._res.get('content-length') == null) {
+                        var len;
+                        if (msg.payload == null) {
+                            len = 0;
+                        } else if (Buffer.isBuffer(msg.payload)) {
+                            len = msg.payload.length;
+                        } else if (typeof msg.payload == "number") {
+                            len = Buffer.byteLength(""+msg.payload);
+                        } else {
+                            len = Buffer.byteLength(msg.payload);
+                        }
+                        msg.res._res.set('content-length', len);
+                    }
+
+                    if (typeof msg.payload === "number") {
+                        msg.payload = ""+msg.payload;
+                    }
+                    msg.res._res.status(statusCode).send(msg.payload);
+                }
+            } else {
+                node.warn(RED._("httpin.errors.no-response"));
+            }
+        });
+    }
+    RED.nodes.registerType("slack response",slackOut);
+
+    // set this to true to spam your console with stuff.
+    var slackDebug = true;
+
+    function slackPost(n) {
+        RED.nodes.createNode(this,n);
+
+        this.channelURL = n.channelURL;
+        this.username = n.username || "";
+        this.emojiIcon = n.emojiIcon || "";
+        this.channel = n.channel || "";
+        var node = this;
+
+        this.on('input', function (msg) {
+            var channelURL = node.channelURL || msg.channelURL;
+            var username = node.username || msg.username;
+            var emojiIcon = node.emojiIcon || msg.emojiIcon;
+            var channel = node.channel || msg.channel;
+
+            var data = {
+                "text": msg.payload,
+                "username": username,
+                "icon_emoji": emojiIcon
+            };
+            if (channel) { data.channel = channel; }
+            if (msg.attachments) { data.attachments = msg.attachments; }
+            if (slackDebug) { node.log(JSON.stringify(data)); }
+            try {
+                request({
+                    method: 'POST',
+                    uri: channelURL,
+                    body: JSON.stringify(data)
+                });
+            }
+            catch (err) {
+                console.trace();
+                node.log(err,msg);
+            }
+        });
+    }
+    RED.nodes.registerType("slack post", slackPost);
+
+};
